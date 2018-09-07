c2727b5e24efef3fe92feb0f63fd5309bd497fd5
“Sam <sammans@Sams-MBP.fios-router.home>
2018-09-07 18:12:51 -0400

    Resized main image and made it responsive, reduced padding between sections, added js to clear input placeholder text

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
   console.log('Document is ready');
   const navButton = $('button.navbar-toggle');
   const collapsingDiv = $('div.navbar-collapse');
-  
+  const input = $('div.input-group > input, textarea');
+
  /* When user scrolls, the nav tab disappears */
  navButton.click(function(){
-  
+
  const changeClass = function(){
    /* Checks if css is set certain class when menu is displaying */
     if(collapsingDiv.hasClass('in')) {
@@ -16,6 +17,16 @@ $(document).ready(function(){
  }//function ends
    /*When you scroll the code below runs */
    $(window).scroll(changeClass)
+
+ })
+
+/* Clear placeholder text when user clicks into the textbox and add it when they exit it*/
+ input.focus(function(){
+   const placeholderText = this.placeholder;
+   this.placeholder = "";
+    input.blur(function(){
+     this.placeholder = placeholderText;
+   })
 
  })
 
