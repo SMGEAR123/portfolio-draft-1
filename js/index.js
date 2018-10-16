c61e41f0477d508b19eb09d5e5fa12af61f57bcd
“Sam <sammans@Sams-MBP.fios-router.home>
2018-10-16 12:04:01 -0400

    Added hover effect to social links and resized social links and shortcut  elements

--- js/index.js
+++ js/index.js
@@ -10,20 +10,19 @@ anchorLinks.fadeOut();//Links fade out at the beginning
 
  /* When user scrolls, the nav tab disappears */
  navButton.click(function(){
-
- const changeClass = function(){
+   const changeClass = function(){
    /* Checks if css is set certain class when menu is displaying */
     if(collapsingDiv.hasClass('in')) {
       /*Clicks and returns when criteria is met*/
       navButton.trigger('click');
       return;
     }
- }//function ends
+ }//nav click function ends
 
 /*When you scroll the code below runs */
-   $(window).scroll(changeClass)
+    $(window).scroll(changeClass)
 
- })
+ })//navButton Click event ends
 
 /* Create a function that displays shortcut anchor links */
  shortcutBtn.click(function() {
@@ -42,6 +41,8 @@ anchorLinks.fadeOut();//Links fade out at the beginning
 
  })
 
+
+
 /* Clear placeholder text when user clicks into the textbox and add it when they exit it*/
  input.focus(function(){
    const placeholderText = this.placeholder;
@@ -49,7 +50,6 @@ anchorLinks.fadeOut();//Links fade out at the beginning
     input.blur(function(){
      this.placeholder = placeholderText;
    })
-
  })
 
 })//document.ready ends
